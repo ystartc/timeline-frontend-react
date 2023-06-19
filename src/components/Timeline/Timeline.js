e2ad0f45162983c3ef4e9e3e97e100435a4ee953
ystartc <startceva.dev@gmail.com>
2023-06-18 20:03:23 -0700

    Created Timeline.js container component and passed represanATIONAL COMPONENT TO IT ALONG WITH PROPS

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,4 +1,14 @@
 import TimelineEvent from './TimelineEvent/TimelineEvent';
-const Timeline = () => {};
+import './Timeline.css';
+
+const Timeline = props => {
+  return (
+    <TimelineEvent
+      person={props.person}
+      status={props.status}
+      timestamp={props.timastamp}
+    />
+  );
+};
 
 export default Timeline;
